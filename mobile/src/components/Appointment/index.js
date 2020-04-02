f564d1e529290a415300ec4d630448baead83aae
Guilherme Ribas <guilherme.ribas.tech@gmail.com>
2020-04-01 21:11:28 -0300

    Update index.js

--- mobile/src/components/Appointment/index.js
+++ mobile/src/components/Appointment/index.js
@@ -20,7 +20,7 @@ export default function Appointment({ data, onCancel }) {
         <Avatar
           source={{
             uri: data.provider.avatar
-              ? data.provider.avatar.url.replace('localhost', '10.0.2.2')
+              ? data.provider.avatar.url
               : `https://api.adorable.io/avatar/50/${data.provider.name}.png`,
           }}
         />
